2971b82ecdf2209fa099fe8b809f1dd9b815d720
Ben Bowes <ben.bowes@rea-group.com>
2017-01-20 03:54:34 +1100

    Refactor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var SelectBox = function(element) {
 
   this.domRefs = {
-    listBox: element,
+    selectBox: element,
     optionNodes: element.querySelectorAll('.option'),
     label: element.querySelector('.label'),
     hiddenInputValue: element.querySelector('input[name]')
@@ -19,8 +19,8 @@ var SelectBox = function(element) {
 
   // Bind functions for listeners
   this.handleListBoxOnBlur = this.handleListBoxOnBlur.bind(this);
-  this.listItemKeyEvent = this.listItemKeyEvent.bind(this);
-  this.listItemClick = this.listItemClick.bind(this);
+  this.handleOptionNodeKeyEvent = this.handleOptionNodeKeyEvent.bind(this);
+  this.handleOptionNodeClick = this.handleOptionNodeClick.bind(this);
 
   // Initialise
   this.addListeners();
@@ -31,41 +31,41 @@ SelectBox.prototype = {
 
   updateState: function(action) {
     switch(action.type) {
-      case 'SET_OPTIONS_OPEN':
-        return this.state.optionsOpen = action.value;
-
-      case 'SET_SELECTED_INDEX':
-        return this.state.selectedIndex = action.value;
-
-      case 'SET_LAST_SELECTED_INDEX':
-        return this.state.lastSelectedIndex = action.value;
+      case 'SET_OPTIONS_OPEN': return this.state.optionsOpen = action.value;
+      case 'SET_SELECTED_INDEX': return this.state.selectedIndex = action.value;
+      case 'SET_LAST_SELECTED_INDEX': return this.state.lastSelectedIndex = action.value;
     }
   },
 
   addListeners: function() {
-    this.domRefs.listBox.addEventListener('touchstart', this.listItemClick, false);
-    this.domRefs.listBox.addEventListener('mousedown', this.listItemClick, false);
-    this.domRefs.listBox.addEventListener('keydown', this.listItemKeyEvent, false);
-    this.domRefs.listBox.addEventListener('blur', this.handleListBoxOnBlur, false);
+    this.domRefs.selectBox.addEventListener('touchstart', this.handleOptionNodeClick, false);
+    this.domRefs.selectBox.addEventListener('mousedown', this.handleOptionNodeClick, false);
+    this.domRefs.selectBox.addEventListener('keydown', this.handleOptionNodeKeyEvent, false);
+    this.domRefs.selectBox.addEventListener('blur', this.handleListBoxOnBlur, false);
   },
 
   updateUI: function() {
-    this.domRefs.optionNodes[this.state.lastSelectedIndex].classList.remove('selected');
-    this.domRefs.optionNodes[this.state.selectedIndex].classList.add('selected');
-    this.domRefs.label.textContent = this.domRefs.optionNodes[this.state.selectedIndex].textContent;
-    this.domRefs.hiddenInputValue.value = this.domRefs.optionNodes[this.state.selectedIndex].getAttribute('data-value');
+    var selectedOption = this.domRefs.optionNodes[this.state.selectedIndex];
+    var previousOption = this.domRefs.optionNodes[this.state.lastSelectedIndex];
+
+    // Toggle option classes
+    previousOption.classList.remove('selected');
+    selectedOption.classList.add('selected');
+
+    // Scroll to keep the selected option in view
+    selectedOption.parentNode.scrollTop = selectedOption.offsetTop;
+
+    // Set label and select-box form vale
+    this.domRefs.label.textContent = selectedOption.textContent;
+    this.domRefs.hiddenInputValue.value = selectedOption.getAttribute('data-value');
     this.updateState({ type: 'SET_LAST_SELECTED_INDEX', value: this.state.selectedIndex });
   },
 
   getInitialSelectedOptionIndex: function() {
-    var initialSelectedOption = this.domRefs.listBox.querySelector('.option.selected');
+    var initialSelectedOption = this.domRefs.selectBox.querySelector('.option.selected');
     return (initialSelectedOption)
       ? this.getChildIndex(initialSelectedOption)
       : 0;
-  },
-
-  handleListBoxOnBlur: function() {
-    this.toggleOptionsPanel('closed');
   },
 
   getNextIndex: function(mode) {
@@ -92,34 +92,65 @@ SelectBox.prototype = {
     }
   },
 
-  listItemKeyEvent: function(e) {
+  getChildIndex: function(element) {
+    // Find index of `child` relative to `parent`
+    return Array.prototype.indexOf.call(element.parentNode.children, element);
+  },
+
+  toggleOptionsPanel: function(mode) {
+    switch (mode) {
+      case 'open':
+        this.updateState({ type: 'SET_OPTIONS_OPEN', value: true });
+        return this.domRefs.selectBox.classList.add('options-visible');
+
+      case 'close':
+        this.updateState({ type: 'SET_OPTIONS_OPEN', value: false });
+        return this.domRefs.selectBox.classList.remove('options-visible');
+
+      default:
+        if (this.state.optionsOpen === false) {
+          this.updateState({ type: 'SET_OPTIONS_OPEN', value: true });
+          return this.domRefs.selectBox.classList.add('options-visible');
+        } else {
+          this.updateState({ type: 'SET_OPTIONS_OPEN', value: false });
+          return this.domRefs.selectBox.classList.remove('options-visible');
+        }
+    }
+  },
+
+  // HANDLERS
+
+  handleOptionNodeKeyEvent: function(e) {
     if (e.keyCode === 40 || e.keyCode === 38 || e.keyCode === 13) {
-      e.preventDefault(); // Disable keyboard arrow scroll functionality
+      e.preventDefault(); // Disable native functionality
     }
 
     switch (e.keyCode) {
       case 13: // Enter
         return this.toggleOptionsPanel();
 
-      case 40: // Down
-        this.updateState({ type: 'SET_SELECTED_INDEX', value: this.getNextIndex('increment') });
-        if (this.state.optionsOpen === false) this.toggleOptionsPanel('open');
-        return this.updateUI();
+      case 27: // Esc
+        return this.domRefs.selectBox.blur();
 
       case 38: // Up
         this.updateState({ type: 'SET_SELECTED_INDEX', value: this.getNextIndex('decrement') });
         if (this.state.optionsOpen === false) this.toggleOptionsPanel('open');
         return this.updateUI();
+
+      case 40: // Down
+        this.updateState({ type: 'SET_SELECTED_INDEX', value: this.getNextIndex('increment') });
+        if (this.state.optionsOpen === false) this.toggleOptionsPanel('open');
+        return this.updateUI();
     }
   },
 
-  listItemClick: function(e) {
+  handleOptionNodeClick: function(e) {
     // There are 2 events being fired. mousedown (desktop), touchstart (mobile).
     // Let the 1st cancel the other with `preventDefault`
     e && e.preventDefault();
 
     // no auto focus because `preventDefault` used above
-    this.domRefs.listBox.focus();
+    this.domRefs.selectBox.focus();
 
     // Clicked on a `.option` if its parent is `.options`
     if (e && e.target.parentNode.classList.contains('options')) {
@@ -130,35 +161,15 @@ SelectBox.prototype = {
     this.toggleOptionsPanel();
   },
 
-  getChildIndex: function(element) {
-    // Find index of `child` relative to `parent`
-    return Array.prototype.indexOf.call(element.parentNode.children, element);
-  },
-
-  toggleOptionsPanel: function(mode) {
-    switch (mode) {
-      case 'open':
-        this.updateState({ type: 'SET_OPTIONS_OPEN', value: true });
-        return this.domRefs.listBox.classList.add('options-visible');
-
-      case 'closed':
-        this.updateState({ type: 'SET_OPTIONS_OPEN', value: false });
-        return this.domRefs.listBox.classList.remove('options-visible');
-
-      default:
-        if (this.state.optionsOpen === false) {
-          this.updateState({ type: 'SET_OPTIONS_OPEN', value: true });
-          return this.domRefs.listBox.classList.add('options-visible');
-        } else {
-          this.updateState({ type: 'SET_OPTIONS_OPEN', value: false });
-          return this.domRefs.listBox.classList.remove('options-visible');
-        }
-    }
+  handleListBoxOnBlur: function() {
+    this.toggleOptionsPanel('close');
   }
+
 }
 
-// Setup all .listbox instances
-var listBoxes = document.querySelectorAll('.listbox');
-for(var i = 0; i < listBoxes.length; i++) {
-  new SelectBox(listBoxes[i]);
+// Start
+
+var selectBoxes = document.querySelectorAll('.select-box');
+for(var i = 0; i < selectBoxes.length; i++) {
+  new SelectBox(selectBoxes[i]);
 }
