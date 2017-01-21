4990a55abb095b8f9d08c7ccc537033547a970f4
Ben Bowes <bb@benbowes.com>
2017-01-21 17:52:27 +1100

    Add touchmove detection. Split updateState out to reducer. Add Spacebar check.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import reducer from './reducer';
+
 class SelectBox {
 
   constructor(element) {
@@ -13,12 +15,15 @@ class SelectBox {
     };
 
     this.state = {
+      isDragging: false,
       selectedIndex: this.getInitialSelectedOptionIndex(),
       lastSelectedIndex: 0,
       optionsOpen: false
     };
 
     // Bind functions for listeners
+    this.handleTouchMove = this.handleTouchMove.bind(this);
+    this.handleTouchStart = this.handleTouchStart.bind(this);
     this.handleSelectBoxBlur = this.handleSelectBoxBlur.bind(this);
     this.handleSelectBoxKeyEvent = this.handleSelectBoxKeyEvent.bind(this);
     this.handleSelectBoxClick = this.handleSelectBoxClick.bind(this);
@@ -28,33 +33,26 @@ class SelectBox {
   }
 
   updateState(action) {
-    switch(action.type) {
-
-    case 'SET_OPTIONS_OPEN':
-      return this.state = {
-        ...this.state,
-        optionsOpen: action.value
-      };
-
-    case 'SET_SELECTED_INDEX':
-      return this.state = {
-        ...this.state,
-        selectedIndex: action.value
-      };
-
-    case 'SET_LAST_SELECTED_INDEX':
-      return this.state = {
-        ...this.state,
-        lastSelectedIndex: action.value
-      };
-    }
+    this.state = reducer(this.state, action);
   }
 
   addListeners() {
+    this.domRefs.selectBox.addEventListener('touchmove', this.handleTouchMove, false);
+    this.domRefs.selectBox.addEventListener('touchstart', this.handleTouchStart, false);
     this.domRefs.selectBox.addEventListener('touchend', this.handleSelectBoxClick, false);
     this.domRefs.selectBox.addEventListener('mousedown', this.handleSelectBoxClick, false);
     this.domRefs.selectBox.addEventListener('keydown', this.handleSelectBoxKeyEvent, false);
     this.domRefs.selectBox.addEventListener('blur', this.handleSelectBoxBlur, false);
+  }
+
+  handleTouchStart() {
+    // User has finished dragging
+    this.updateState({ type: 'SET_IS_DRAGGING', value: false });
+  }
+
+  handleTouchMove() {
+    // User is dragging
+    this.updateState({ type: 'SET_IS_DRAGGING', value: true });
   }
 
   updateUI() {
@@ -139,12 +137,16 @@ class SelectBox {
   // HANDLERS
 
   handleSelectBoxKeyEvent(e) {
-    if (e.keyCode === 40 || e.keyCode === 38 || e.keyCode === 13) {
+    if (e.keyCode === 40 || e.keyCode === 38 || e.keyCode === 32 || e.keyCode === 13) {
       e.preventDefault(); // Disable native functionality
     }
 
     switch (e.keyCode) {
     case 13: // Enter
+      e.stopPropagation(); // Do not submit form
+      return this.toggleOptionsPanel();
+
+    case 32: // Space
       return this.toggleOptionsPanel();
 
     case 27: // Esc
@@ -155,8 +157,9 @@ class SelectBox {
         type: 'SET_SELECTED_INDEX',
         value: this.getNextIndex('decrement')
       });
-
-      if (this.state.optionsOpen === false) this.toggleOptionsPanel('open');
+      if (this.state.optionsOpen === false) {
+        this.toggleOptionsPanel('open');
+      }
       return this.updateUI();
 
     case 40: // Down
@@ -164,39 +167,43 @@ class SelectBox {
         type: 'SET_SELECTED_INDEX',
         value: this.getNextIndex('increment')
       });
-
-      if (this.state.optionsOpen === false) this.toggleOptionsPanel('open');
+      if (this.state.optionsOpen === false) {
+        this.toggleOptionsPanel('open');
+      }
       return this.updateUI();
     }
   }
 
   handleSelectBoxClick(e) {
-    // There are 2 events being fired. mousedown (desktop), touchstart (mobile).
-    // Let the 1st cancel the other with `preventDefault`
-    e && e.preventDefault();
+    // Ignore click and touchend if user is dragging
+    if(this.state.isDragging === false) {
 
-    this.domRefs.selectBox.focus();
+      // There are 2 events being fired. mousedown (desktop), touchstart (mobile).
+      // Let the 1st cancel the other with `preventDefault`
+      e && e.preventDefault();
 
-    // Clicked on a `.option` if its parent is `.options`
-    if (e && e.target.parentNode.classList.contains('options-container')) {
-      this.updateState({
-        type: 'SET_SELECTED_INDEX',
-        value: this.getOptionIndex(e.target)
-      });
-      this.updateUI();
+      this.domRefs.selectBox.focus();
+
+      // Clicked on a `.option` if its parent is `.options`
+      if (e && e.target.parentNode.classList.contains('options-container')) {
+        this.updateState({
+          type: 'SET_SELECTED_INDEX',
+          value: this.getOptionIndex(e.target)
+        });
+        this.updateUI();
+      }
+      // Open panel if closed, close panel if open
+      this.toggleOptionsPanel();
     }
-
-    // Open panel if closed, close panel if open
-    this.toggleOptionsPanel();
   }
 
   handleSelectBoxBlur() {
     this.toggleOptionsPanel('close');
   }
-
 }
 
 // START
+// Find elements in DOM with `select-box` class, and applies SelectBox()
 [...document.querySelectorAll('.select-box')].forEach((element) => {
   return new SelectBox(element);
 });
---
+++ src/reducer.js
@@ -0,0 +1,30 @@
+const reducer = (state, action) => {
+  switch(action.type) {
+
+  case 'SET_IS_DRAGGING':
+    return state = {
+      ...state,
+      isDragging: action.value
+    };
+
+  case 'SET_OPTIONS_OPEN':
+    return state = {
+      ...state,
+      optionsOpen: action.value
+    };
+
+  case 'SET_SELECTED_INDEX':
+    return state = {
+      ...state,
+      selectedIndex: action.value
+    };
+
+  case 'SET_LAST_SELECTED_INDEX':
+    return state = {
+      ...state,
+      lastSelectedIndex: action.value
+    };
+  }
+};
+
+export default reducer;
